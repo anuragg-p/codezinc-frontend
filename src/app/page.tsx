826d525c8dad995f53e133030785453e80bfeb9b
anruag <anuragpurohit3773@gmail.com>
2024-10-12 20:55:36 +0530

    : Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function HomePage() {
+  const apps = [
+    { title: "Weather APP", description: "" },
+    { title: "", description: "" },
+    { title: "", description: "" },
+    { title: "", description: "" },
+    { title: "", description: "" },
+  ];
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="">
+        <Card className="w-[300px]">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+        </Card>
+      </div>
+    </main>
+  );
+}
