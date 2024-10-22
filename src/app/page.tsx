11311ad7815f46c545ee6ce3012ff8f3a65665bf
souravgit <souravdash4708@gmail.com>
2024-10-22 12:47:32 +0530

    changed the style from css to tailwind-css and deleted home.module.css file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
-import styles from "../styles/home.module.css"
 import { useRouter } from "next/navigation";
 
 export default function HomePage() {
@@ -16,11 +15,14 @@ export default function HomePage() {
   ];
 
   return (
-    <main className={styles.main}>
-      <h4 className={styles.card_box_heading}>Apps</h4>
-      <div className={styles.cards_container}>
+    <main className="flex flex-col min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white px-[20%]">
+      <h4 className="text-[#FB773C] text-lg font-medium self-start pl-8">Apps</h4>
+      <div className="grid grid-cols-5 gap-8 bg-gradient-to-t from-[#392e6d] to-[#23038e] p-6 border-4 border-[#405D72] shadow-inner shadow-black/50 rounded-[3cap]">
         {apps.map((item, index) => (
-          <Card className={styles.cards} key={index} onClick={() => router.push(item.route)}>
+          <Card 
+          className="h-[250px] w-[200px] text-[#F7E7DC] bg-[#405D72] border-3 border-[#758694] rounded-[3cap] shadow-md shadow-black/40 
+          transition-transform transform hover:scale-110 hover:rounded-[3ch] cursor-pointer" 
+          key={index} onClick={() => router.push(item.route)}>
           <CardHeader>
             <CardTitle>{item.title}</CardTitle>
             <CardDescription>{item.description}</CardDescription>
