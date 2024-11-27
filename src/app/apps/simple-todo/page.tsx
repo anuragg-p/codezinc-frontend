0cada13d89c87330370689b169cd8a5e5d943b48
souravgit <souravdash4708@gmail.com>
2024-11-27 19:35:32 +0530

    added the todos to local storage

--- src/app/apps/simple-todo/page.tsx
+++ src/app/apps/simple-todo/page.tsx
@@ -1,82 +1,74 @@
 "use client";
 import { DashBoardHeader } from "@/components/TodoHeader";
-import {
-  TodoDashBoard,
-  TodoEnum,
-} from "@/components/Todo";
-import tasksData from "@/data/todo-app-data.json";
-import type { Raw, TodoItemType } from "@/types/todo.types";
-import { useCallback, useState } from "react";
+import { TodoDashBoard, TodoEnum } from "@/components/Todo";
+// import tasksData from "@/data/todo-app-data.json";
+import type { TodoItemType } from "@/types/todo.types";
+import { useCallback, useEffect, useState } from "react";
 import { TodoFooter } from "@/components/TodoFooter";
 
-
-// making a function to convert the json file Raw data to our required type data
-const convertToTodoItems = (data: Raw[]): TodoItemType[] => {
-  return data.map((task) => {
-    // Convert the type field to TodoEnum
-    const type = TodoEnum[task.type as keyof typeof TodoEnum];
-    return {
-      type,
-      name: task.name,
-      tags: task.tags
-    };
-  });
-}
-
-// To convert imported json file data to a typed Array<TodoItemType>
-const todoItems: TodoItemType[] = convertToTodoItems(tasksData);
-
+//-----------------------------------------------------------------------------
 export default function TodoPage() {
-  const [todos, setTodos] = useState<Array<TodoItemType>>(todoItems);
+    const [todos, setTodos] = useState<Array<TodoItemType>>(() => {
+        // This is to load data from local storage
+        const storedTodos = localStorage.getItem("todos");
+        return storedTodos ? (JSON.parse(storedTodos) as Array<TodoItemType>) : [];
+    });
 
-  const todayTodos = todos.filter((item) => item.type === TodoEnum.TODAY);
-  const thisWeekTodos = todos.filter(
-    (item) => item.type === TodoEnum.THIS_WEEK,
-  );
-  const eventualTodos = todos.filter(
-    (item) => item.type === TodoEnum.EVENTUALLY,
-  );
+    // Save todos to local storage whenever it changes
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
-  const sections = [
-    {
-      variant: TodoEnum.TODAY,
-      todos: todayTodos,
-    },
-    {
-      variant: TodoEnum.THIS_WEEK,
-      todos: thisWeekTodos,
-    },
-    {
-      variant: TodoEnum.EVENTUALLY,
-      todos: eventualTodos,
-    },
-  ];
+//-----------------------------------------------------------------------------
 
-  // What is useCallback?
-  const addTodo = useCallback((type: TodoEnum, name: string) => {
-    return setTodos((prev) => [...prev, { type, name, tags: [] }]);
-  // }, [todos]);
-  }, []);
+    const todayTodos = todos.filter((item) => item.type === TodoEnum.TODAY);
+    const thisWeekTodos = todos.filter(
+        (item) => item.type === TodoEnum.THIS_WEEK,
+    );
+    const eventualTodos = todos.filter(
+        (item) => item.type === TodoEnum.EVENTUALLY,
+    );
 
-  const removeTodo = useCallback(( name: string) => {
-    return setTodos((prev) => prev.filter(todo => todo.name !== name));
-  }, []);
+    const sections = [
+        {
+            variant: TodoEnum.TODAY,
+            todos: todayTodos,
+        },
+        {
+            variant: TodoEnum.THIS_WEEK,
+            todos: thisWeekTodos,
+        },
+        {
+            variant: TodoEnum.EVENTUALLY,
+            todos: eventualTodos,
+        },
+    ];
 
-  return (
-    <main className="flex min-h-screen flex-col bg-gray-900 px-4">
-      <DashBoardHeader />
-      <section className="m-4 flex flex-grow flex-wrap gap-4 p-4">
-        {sections.map((item, index) => (
-          <TodoDashBoard
-            key={index}
-            variant={item.variant}
-            todos={item.todos}
-            addTodo={addTodo}
-            removeTodo={removeTodo}
-          />
-        ))}
-      </section>
-      <TodoFooter />
-    </main>
-  );
+    // What is useCallback?
+    const addTodo = useCallback((type: TodoEnum, name: string) => {
+        return setTodos((prev) => [...prev, { type, name, tags: [] }]);
+        // }, [todos]);
+    }, []);
+
+    const removeTodo = useCallback((name: string) => {
+        return setTodos((prev) => prev.filter((todo) => todo.name !== name));
+    }, []);
+
+    return (
+        <main className="flex min-h-screen flex-col bg-gray-900 px-4">
+            <DashBoardHeader />
+            <section className="m-4 flex flex-grow flex-wrap gap-4 p-4">
+                {sections.map((item, index) => (
+                    <TodoDashBoard
+                        key={index}
+                        variant={item.variant}
+                        todos={item.todos}
+                        addTodo={addTodo}
+                        removeTodo={removeTodo}
+                    />
+                ))}
+            </section>
+            <TodoFooter />
+        </main>
+    );
 }
